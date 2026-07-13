54d267aefb3a3242397388fcc1d39eb2fd238c20
rakib97j <rakibhassan23012007@gmail.com>
2026-07-13 15:20:39 +0600

    Banner Card Added

--- src/components/BannerCard.jsx
+++ src/components/BannerCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BannerCard = () => {
     return <section className="bg-[#8cceda60]">
         <div className='py-14'>
-        Banner Card 
+        <h1>Banner Card Particle </h1>
         </div>
     </section>;
 };
