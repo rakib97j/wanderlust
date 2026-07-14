3f24af017238892f556d5c3c500f88680676e9b5
rakib97j <rakibhassan23012007@gmail.com>
2026-07-14 21:44:16 +0600

    assss

--- src/components/BannerCard.jsx
+++ src/components/BannerCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BannerCard = () => {
     return <section className="bg-[#8cceda60]">
         <div className='py-14'>
-        <h1>Banner Card Particle Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio saepe deleniti optio officia accusamus magni voluptate, deserunt culpa quaerat. Esse voluptatum aliquid, libero consequuntur vero in. Laboriosam enim ab iure? </h1>
+        <h1>Banner  </h1>
         </div>
     </section>;
 };
